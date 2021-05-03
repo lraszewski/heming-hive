6c9c07bf69b158c8ba986a6de60675f8effdd8cb
Luc Raszewski <ls.ki@outlook.com>
2021-05-03 20:42:51 +1000

    allowed db to use grid fs to upload large files

--- src/db.js
+++ src/db.js
@@ -1,5 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require("mongoose");
+const grid = require('gridfs-stream');
+const mgfs = require('multer-gridfs-storage');
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+
+grid.mongo = mongoose.mongo
 
 mongoose.connect(
 	process.env.CONNECTION_STRING,
@@ -16,8 +23,31 @@ db.on("error", err => {
 	process.exit(1);
 })
 
+let gfs
 db.once("open", async () => {
+	gfs = grid(db.db);
+	gfs.collection('videos');
 	console.log("db connection started on " + db.host + ":" + db.port);
 })
 
-//require("./models/*");
+const storage = new mgfs({
+	db: db,
+	file: (req, file) => {
+		return new Promise((resolve, reject) => {
+			crypto.randomBytes(16, (err, buf) => {
+				if (err) {
+					return reject(err);
+				}
+				const filename = buf.toString('hex') + path.extname(file.originalname);
+				const fileInfo = {
+					filename: filename,
+					bucketName: 'videos'
+				};
+				resolve(fileInfo);
+			})
+		})
+	}
+});
+const upload = multer({ storage });
+
+exports.upload = upload;
