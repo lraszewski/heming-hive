4b91f19d678869640e8a2f49250d148f7c8489fe
Luc Raszewski <ls.ki@outlook.com>
2021-05-03 17:06:31 +1000

    updated user router with new paths and permissions

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,11 +5,17 @@ const userController = require('../controllers/userController');
 router.get('/register', (req, res) => {
     res.render('../views/user/register');
 });
-router.post('/register', userController.register);
+router.post('/register', userController.createUser);
 
 router.get('/login', (req, res) => {
 	res.render('../views/user/login');
 })
 router.post('/login', userController.login);
+
+router.get('/user/:userId', userController.allowIfLoggedIn, userController.readUser);
+router.put('/user/:userId', userController.allowIfLoggedIn, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
+router.delete('/user/:userId', userController.allowIfLoggedIn, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
+ 
+router.get('/users', userController.allowIfLoggedIn, userController.grantAccess('readAny', 'profile'), userController.readUsers);
 
 module.exports = router;
