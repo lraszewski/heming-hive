67b74a42964153db31bcaf3b7b6072c52b170f80
Luc Raszewski <ls.ki@outlook.com>
2021-05-03 17:06:01 +1000

    updated app.js to user userRouter and ejs view engine

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ const mongoose = require('mongoose');
 const app = express();
 dotenv.config();
 require('./db.js');
+app.set('view engine', 'ejs');
+
+const userRouter = require('./routes/userRouter');
+
+app.use('/', userRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-	console.log("server is listening on port: " + port);
+	console.log("server is listening on port " + port);
 });
