36e51ddf04d5ba961cba3a0fbcc7b480ac72db17
Luc Raszewski <ls.ki@outlook.com>
2021-05-04 17:08:04 +1000

    added lesson/ method

--- src/routes/lessonRouter.js
+++ src/routes/lessonRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const lessonController = require('../controllers/lessonController');
+const userController = require('../controllers/userController');
 const upload = require('../db').upload;
 
 router.get('/create', (req, res) => {
     res.render('../views/lesson/create');
 });
 router.post('/create', upload.single('file'), lessonController.createLesson);
+
+router.get('/', userController.allowIfLoggedIn, userController.grantAccess('readAny', 'lesson'), lessonController.readLessons);
 
 module.exports = router;
