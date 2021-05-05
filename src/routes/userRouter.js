d2d8c5df117a2ac5afa85cf3451082c763fd63fa
Luc Raszewski <ls.ki@outlook.com>
2021-05-05 21:19:15 +1000

    reworked user router to use new passport authentication functions

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/register', (req, res) => {
+router.get('/register', userController.checkNotAuthenticated, (req, res) => {
     res.render('../views/user/register');
 });
 router.post('/register', userController.createUser);
 
-router.get('/login', (req, res) => {
+router.get('/login', userController.checkNotAuthenticated, (req, res) => {
 	res.render('../views/user/login');
 })
 router.post('/login', userController.login);
 
-router.get('/:userId', userController.allowIfLoggedIn, userController.readUser);
-router.put('/:userId', userController.allowIfLoggedIn, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
-router.delete('/:userId', userController.allowIfLoggedIn, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
+router.get('/:userId', userController.checkAuthenticated, userController.readUser);
+router.put('/:userId', userController.checkAuthenticated, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
+router.delete('/:userId', userController.checkAuthenticated, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
  
-router.get('/', userController.allowIfLoggedIn, userController.grantAccess('readAny', 'profile'), userController.readUsers);
+router.get('/', userController.checkAuthenticated, userController.grantAccess('readAny', 'profile'), userController.readUsers);
 
 module.exports = router;
