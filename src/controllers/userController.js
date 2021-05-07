1db47dbb31e99a41744387823f2f09df0dd1ee1d
Luc Raszewski <ls.ki@outlook.com>
2021-05-07 16:49:24 +1000

    fixed incorrect failure redirect path

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -93,7 +93,7 @@ async function deleteUser(req, res, next) {
 async function login(req, res, next) {
 	passport.authenticate('local', {
 		successRedirect: '/',
-		failureRedirect: '/login'
+		failureRedirect: '/user/login'
 	})(req, res, next);
 }
 
