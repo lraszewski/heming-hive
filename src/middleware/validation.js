daf831b6f692a9345e3eca524a49a53faa56fe9c
Luc Raszewski <ls.ki@outlook.com>
2021-05-07 20:45:00 +1000

    created validation for lessons and users

---
+++ src/middleware/validation.js
@@ -0,0 +1,33 @@
+const joi = require('joi');
+
+const userValidation = data => {
+    const schema = joi.object({
+        email: joi.string()
+            .max(255)
+            .required()
+            .email(),
+        password: joi.string()
+            .min(6)
+            .max(255)
+            .required(),
+		role: joi.string()
+			.valid('user', 'student', 'admin')
+    });
+    return schema.validate(data);
+};
+
+const lessonValidation = data => {
+    const schema = joi.object({
+        title: joi.string()
+            .max(255)
+            .required(),
+        description: joi.string()
+            .required(),
+		video: joi.string()
+            .required(),
+    });
+    return schema.validate(data);
+}
+
+module.exports.userValidation = userValidation;
+module.exports.lessonValidation = lessonValidation;
